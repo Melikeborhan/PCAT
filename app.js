503e5dbd75e9f1e3115cdd3c4d628379ba21f963
Melike <22410051025@ogr.erbakan.edu.tr>
2024-02-17 22:27:01 +0300

    Static files uploads

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function(){
-    console.log('Function working');
-};
+const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
+
+const app = express();
+
+// Statik dosyaları "public" dizininden sunun
+app.use(express.static('temp'));
+
+// Ana rota için "temp/index.html" dosyasını gönderin
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} numaralı portta çalışıyor.`);
+});
